30ec673410fd211b6a67ce2c3a6beaec69b50abb
Dylan Miracle <dylan.miracle@gmail.com>
2020-11-04 09:27:28 -0600

    add remaining files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Project starter
+
+## Steps
+1. create git repo
+    - gitignore
+1. download boilerplate
+1. create and push remote
+
+## Variables
+1. repo-name
+1. description
+1. 
+
+## Environmental Vars
+Make a `.env` file. Add your tokens
+```
+GITHUB_TOKEN=<token>
+AWS_KEY_ID=<>
+AWS_SECRET_KEY=<>
+```
+
+## Ansible
+```
+python3 -m pip install ansible
+
+```
+
+## To do
+
+- add dev container branch
+- add boiler plate projects
+- dry-run mode
+- cli
---
+++ projectrTest.py
@@ -0,0 +1,40 @@
+import unittest
+import githubutil as gh
+import yamlutil as yu
+
+class TestProject(unittest.TestCase):
+
+    yu_agent = yu.YamlUtil()
+    gh_agent = gh.GithubUtil()
+    
+    @unittest.skip
+    def test_load_config(self):
+        self.yu_agent.load("config.yml")
+
+    @unittest.skip
+    def test_create_repo(self):
+        r = self.gh_agent.createRepo("t04")
+        print(r)
+
+    @unittest.skip("This is too much power")
+    def test_delete_repo(self):
+        r = self.gh_agent.deleteRepo("dmiracle", "testymctestface")
+        print(r.status_code)
+        print(r.text)
+
+    @unittest.skip
+    def test_clone_repo(self):
+        ec, path = self.gh_agent.cloneRepo('https://github.com/dmiracle/jupyter-starter.git', 'temp')
+        print(ec)
+        print(path)
+
+    @unittest.skip
+    def test_change_remote(self):
+        ec, path = self.gh_agent.cloneRepo('https://github.com/dmiracle/jupyter-starter.git', 'temp')
+        print(ec)
+        print(path)
+        out = self.gh_agent.changeRemote(path, 'test2')
+        print(out)
+
+if __name__ == '__main__':
+    unittest.main()
